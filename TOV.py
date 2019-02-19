092fb28ee066f01a41f52e999375b65c46fa7694
Juan Fernández <a01196685@itesm.mx>
2019-02-19 13:44:46 +0800

    Create TOV.py

---
+++ TOV.py
@@ -0,0 +1,24 @@
+# polytopic_eos.py contains function that returns the value of the specific internal energy of a polytopic fluid
+# outlined as e = P / (y - 1)*rho , where P is some constant, y is the polytopic 
+
+
+
+def convert(G,c,epsilon,)
+
+
+def dp():
+    -G*(rho*(1+epsilon/c**2))
+    
+
+
+def Gmass(r):
+    return 4*pi*r**2*rho*(1-epsilon/(c**2))
+    
+def Bmass():
+    return 4*pi*r**2*rho*sqrt(det(g))
+    #return (sqrt(1-2*G*M/(r*c**2)))**(-1)*4*pi*r^2*rho
+    
+
+    
+def drho():
+    
