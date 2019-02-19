7cf8a8817740c9d132ee27cbe4ab1cfcb68e772a
Juan Fernández <a01196685@itesm.mx>
2019-02-19 13:59:40 +0800

    Update TOV.py

--- TOV.py
+++ TOV.py
@@ -2,22 +2,23 @@
 # outlined as e = P / (y - 1)*rho , where P is some constant, y is the polytopic 
 
 
-def convert(G,c,epsilon,)
+# Function that takes all quantities and converts them all to computational units
+#def convert(G,c,epsilon,) 
 
-
-def dp():
-    -G*(rho*(1+epsilon/c**2))
+def dpdr(rho,epsilon,p,m):
+    return -G*(rho*(1+epsilon/c**2)+p/c**2)*(m+4*pi*r**3*p/c**2)/(r*(r-2*G*m/c**2))
     
-
-
-def Gmass(r):
-    return 4*pi*r**2*rho*(1-epsilon/(c**2))
+def dmdr(r,rho,epsilon):
+    return 4*pi*r**2*rho*(1+epsilon/(c**2))
     
-def Bmass():
-    return 4*pi*r**2*rho*sqrt(det(g))
-    #return (sqrt(1-2*G*M/(r*c**2)))**(-1)*4*pi*r^2*rho
+def dphidr(m,r,p):
+    return (m+4*pi*r**3*p/c**2)/(r*(r-2*G*m/c**2))
     
-
+def Gmass(r,rho,epsilon): # integrand
+    return 4*pi*r**2*rho*(1+epsilon/(c**2))
     
-def drho():
+def Bmass(r,rho,M): # integrand, M: total mass
+    #return 4*pi*r**2*rho*sqrt(det(g))
+    return (sqrt(1-2*G*M/(r*c**2)))**(-1)*4*pi*r^2*rho
+    
     
