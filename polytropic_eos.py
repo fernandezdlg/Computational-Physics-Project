e59b4883ccf2582485a3e7a6926e5a28a6561035
Aaron Elijah <aaronzakelijah@googlemail.com>
2019-03-23 21:35:23 +0800

    Begun to add Max Compact equations

--- polytropic_eos.py
+++ polytropic_eos.py
@@ -1,7 +1,7 @@
 # polytopic_eos.py contains function that returns the value of the specific internal energy of a polytopic fluid
 # outlined as e = P / (y - 1)*rho , where P is some constant, y is the polytopic constant
 
-### POLYTROPIC EOS equations
+### polytropic EOS equations
 # return specific energy from pressure p , density rho and gamma constant
 def sp_energy_eos(p, rho, gamma):
     return p / ((gamma - 1)*rho) 
@@ -12,4 +12,5 @@ def pressure_eos(rho, K, gamma):
 def rho_eos(p, K, gamma):
     return (p / K)**(1/gamma)
 
-### may want to add more EoS in the future
+### Maximally compact EOS equations
+def 
